8b3f69ec1bf304785bef00de72731e8f439de4dd
lshn1007 <lshn1007@hanyang.ac.kr>
2021-03-11 23:08:29 +0900

    21-03-11

---
+++ test_002/src/components/seunghwan.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <p>{{ name }}</p>
+        <button @click='updateName' v-if='seen'></button>
+    </div>
+</template>
+
+<script> 
+    export default{
+        data(){
+            return {
+                name : '변경 전입니다',
+                seen : true
+            }
+        },
+        methods: {
+            updateName(){
+                if(this.seen === false){
+                    this.name = '복귀했습니다.'
+                }
+                else{
+                    this.name = '변경되었스빈다'
+                }
+            }
+        }
+    }
+</script>
+<style scoped>
+</style>
+
---
+++ test_002/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>seunghwan coder</h1>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <seunghwan/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+import seunghwan from '@/components/seunghwan.vue';
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    seunghwan
+  }
+}
+</script>
+
+
+<style scoped>
+h1{
+  color : red;
+}
+</style>
